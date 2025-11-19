c7b85bef09f1d085ede03074662992ecd193b763
Zlatagaga <zlatasparrow@gmail.com>
2025-11-18 17:30:14 -0800

    dag4 decorator

--- dags/my_dag4.py
+++ dags/my_dag4.py
@@ -1,29 +1,32 @@
-from airflow.sdk import dag, DAG, task
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import dag, task
 from pendulum import datetime
 
+@dag(
+    dag_id="my_dag4",
+    schedule="@daily",
+    start_date=datetime(2025, 1, 1),
+    description="A simple DAG with task dependencies",
+    tags=["example"],
+)
 def my_dag():
 
     @task
     def task_a():
         print("Hello from task_a")
-    task_a()
 
     @task
     def task_b():
         print("Hello from task_b")
-    task_b()
 
     @task
     def task_c():
         print("Hello from task_c")
-    task_c()
 
     @task
     def task_d():
         print("Hello from task_d")
-    task_d()
 
+    # Set task dependencies
     task_a() >> task_b() >> task_c() >> task_d()
 
 my_dag()
